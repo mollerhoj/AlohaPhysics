63566ad42064649e759b21a85f75e59d0c58a6f8
Jens Mollerhoj <mollerhoj3@gmail.com>
2012-04-15 14:08:42 -1000

    All files from the projects commit

---
+++ AlohaPhysics/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  AlohaPhysics
+//
+//  Created by Jens Møllerhøj on 15/04/2012.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
---
+++ AlohaTester/AlohaTester.h
@@ -0,0 +1,13 @@
+//
+//  AlohaTester.h
+//  AlohaTester
+//
+//  Created by Jens Møllerhøj on 15/04/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface AlohaTester : SenTestCase
+
+@end
